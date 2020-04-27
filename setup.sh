0134bf0bc50523bfb6799ec3ea50f51c4cf5f03c
Gospodin Sreda <63187920+gospodinsreda@users.noreply.github.com>
2020-04-27 19:13:32 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt install python3-pip
-sudo apt install xvfb
+sudo apt install -y xvfb
 sudo apt-get install x11-utils
 sudo apt install firefox
 sudo apt install firefox-geckodriver
