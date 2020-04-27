667e1af6c01bb85052e93a09716bd09aa04b566c
Gospodin Sreda <63187920+gospodinsreda@users.noreply.github.com>
2020-04-27 19:13:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,4 @@ sudo apt install xvfb
 sudo apt-get install x11-utils
 sudo apt install firefox
 sudo apt install firefox-geckodriver
-sudo mv geckodriver /usr/local/bin/
 sudo apt install tesseract-ocr
