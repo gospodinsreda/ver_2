78bedbd6e3039048ca9c3856c413f20721784c8f
Gospodin Sreda <63187920+gospodinsreda@users.noreply.github.com>
2020-04-27 19:20:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
-sudo apt install python3-pip
-sudo apt install -y xvfb
+sudo apt-get install python3-pip
+sudo apt-get install -y xvfb
 sudo apt-get install x11-utils
-sudo apt install firefox
-sudo apt install firefox-geckodriver
-sudo apt install tesseract-ocr
+sudo apt-get install firefox
+sudo apt-get install firefox-geckodriver
+sudo apt-get install tesseract-ocr
