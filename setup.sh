14082c118cd1621d0ec6d1b82324f5f5f6f466cc
Gospodin Sreda <63187920+gospodinsreda@users.noreply.github.com>
2020-04-27 19:12:53 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt install python3-pip
+sudo apt install xvfb
+sudo apt-get install x11-utils
+sudo apt install firefox
+sudo apt install firefox-geckodriver
+sudo mv geckodriver /usr/local/bin/
+sudo apt install tesseract-ocr
