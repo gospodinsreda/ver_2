d97ec8f0363cae2059097b1c0dbfa333edf6c1ea
Gospodin Sreda <63187920+gospodinsreda@users.noreply.github.com>
2020-07-09 18:22:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ ADMINS = [579042325, 217166737, 660650174, 582250253]
 WINDOWS = False
 SLEEP_TIME = 45
 proxy = {'https': 'socks5h://579042325:3PLPKmAK@orbtl.s5.opennetwork.cc:999'}
-DEBUG = True
+DEBUG = False
 AVITO_EXAMPLE_LINK = 'https://m.avito.ru/moskva/bytovaya_tehnika/dlya_kuhni/holodilniki_i_morozilnye_kamery?priceMax=2000&sort=date&withImagesOnly=false&withDeliveryOnly=false'
 YOLA_EXAMPLE_LINK = 'https://youla.ru/all/bytovaya-tekhnika/holodilniki?attributes[holodilniki_tip][0]=10010&attributes[holodilniki_tip][1]=10011&attributes[holodilniki_tip_ustanovki][0]=10012&attributes[holodilniki_tip_ustanovki][1]=10013&attributes[price][to]=200000&attributes[sort_field]=date_published'
 ADMIN_WA_CHAT_ID = '79153396060@c.us'
